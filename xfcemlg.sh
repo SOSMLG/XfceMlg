6de0921de3d7d8d818b3666de203b4c7725a235c
SOSMLG <117748270+SOSMLG@users.noreply.github.com>
2025-03-28 22:15:54 +0100

    Update xfcemlg.sh

--- xfcemlg.sh
+++ xfcemlg.sh
@@ -1,3 +1,4 @@
 sudo apt purge firefox 
 sudo apt autoremove
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+#paste this in xfce4-pannel 0a0b12
