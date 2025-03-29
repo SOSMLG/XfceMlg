456a5d2bd6a0c304dc3d1b5c5ba67d041f626d48
SOSMLG <117748270+SOSMLG@users.noreply.github.com>
2025-03-29 13:15:56 +0100

    Update xfcemlg.sh

--- xfcemlg.sh
+++ xfcemlg.sh
@@ -1,5 +1,7 @@
 sudo apt purge firefox 
 sudo apt autoremove
-sudo apt install firefox-esr vlc fonts-font-awesome fonts-terminus telegram-desktop exa zoxide  linux-headers-amd64 firmware-iwlwifi firmware-misc-nonfree 
+sudo apt install firefox-esr vlc fonts-font-awesome fonts-terminus telegram-desktop exa zoxide  linux-headers-amd64 firmware-iwlwifi firmware-misc-nonfree libreoffice
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 #paste this in xfce4-pannel 0a0b12
+sudo cp gtk.css ~/.config/gtk-3.0/gtk.css
+sudo cp terminalrc ~/.config/xfce4/terminal/terminalrc
