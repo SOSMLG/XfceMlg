b8fe91f2a1bbf976452671e96747e07757979d07
SOSMLG <117748270+SOSMLG@users.noreply.github.com>
2025-04-04 22:19:57 +0100

    Update xfcei3wm.sh

--- xfcei3wm.sh
+++ xfcei3wm.sh
@@ -1,3 +1,3 @@
 # removing uncessary files 
 sudo apt remove --purge xfdesktop4
-sudo apt remove -y xfdesktop4 xfwm4  xfce4-settings xfce4-session  i3blocks dmenu && sudo apt autoremove -y
+sudo apt remove -y xfdesktop4 xfwm4  xfce4-settings i3blocks dmenu && sudo apt autoremove -y
