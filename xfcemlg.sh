229505bc89ba52dafd6f93e168895dce4eeeaa33
SOSMLG <117748270+SOSMLG@users.noreply.github.com>
2025-04-20 22:33:04 +0100

    Update xfcemlg.sh

--- xfcemlg.sh
+++ xfcemlg.sh
@@ -5,7 +5,7 @@
 sudo apt purge firefox 
 sudo apt autoremove
 # installing ohmybash and replacing firefox with firefox-esr
-sudo apt install firefox-esr vlc fonts-font-awesome fonts-terminus telegram-desktop exa zoxide  linux-headers-amd64 firmware-iwlwifi firmware-misc-nonfree libreoffice fonts-noto-extra fonts-noto-color-emoji i3-wm
+sudo apt install firefox-esr vlc fonts-font-awesome  exa zoxide  linux-headers-amd64 firmware-iwlwifi firmware-misc-nonfree libreoffice fonts-noto-extra fonts-noto-color-emoji i3-wm
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 #paste this in xfce4-pannel 0a0b12
 #pasting my own config 
@@ -35,34 +35,3 @@ cd ..
 rm -rf Orchis-theme Colloid-icon-theme
 
 echo "✅ Installation complete! Apply the themes using your system's appearance settings."
-# fonts 
-#!/bin/bash
-
-# Define font directories for user and root
-USER_FONT_DIR="$HOME/.local/share/fonts"
-ROOT_FONT_DIR="/root/.local/share/fonts"
-
-# Create font directories if they don't exist
-mkdir -p "$USER_FONT_DIR"
-sudo mkdir -p "$ROOT_FONT_DIR"
-
-# List of essential Nerd Fonts
-FONTS=("JetBrainsMono" "FiraCode" "Hack" "DejaVuSansMono" "UbuntuMono")
-
-# Download and install fonts
-for FONT in "${FONTS[@]}"; do
-    echo "Downloading $FONT Nerd Font..."
-    wget -q --show-progress "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/${FONT}.zip" -O "/tmp/${FONT}.zip"
-    
-    echo "Extracting $FONT..."
-    unzip -q "/tmp/${FONT}.zip" -d "$USER_FONT_DIR"
-    
-    # Copy to root's font directory as well
-    sudo cp -r "$USER_FONT_DIR" "$ROOT_FONT_DIR"
-
-    # Remove zip file to save space
-    rm "/tmp/${FONT}.zip"
-done
-
-
-
